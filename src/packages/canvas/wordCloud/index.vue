1765e2a0bbef0f42598a7c8609ab36a1e9cd81ca
黑咖啡 <2452052376@qq.com>
2020-07-31 10:54:27 +0800

    fix: 词云组件

--- src/packages/canvas/wordCloud/index.vue
+++ src/packages/canvas/wordCloud/index.vue
@@ -2,10 +2,11 @@
     <div ref="wordCloud" class="wordCloud">
         wordCloud组件
         <div id="chart"></div>
+        <!-- <canvas id="canvas" width="400px" height="400px"></canvas> -->
     </div>
 </template>
 <script>
-import * as wordCloud from './lib/wordcloud2.min.js'
+import './lib/js2wordcloud.min.js'
 export default {
     name: 'wordCloud',
     props: {},
@@ -15,16 +16,65 @@ export default {
             keywords: {
                 test: 122,
                 你好: 20,
-                中国: 100
+                中国: 100,
+                测试: 10,
+                周五: 90,
+                周四: 40,
+                周三: 35,
+                周二: 30,
+                周一: 55,
+                周末: 90,
+                蘑菇: 80,
+                penggu: 200,
+                sai: 34,
+                shikong: 24,
+                tokui: 62,
+                sokui: 25,
+                loukst: 35,
+                tukio: 66
             }
         }
     },
     computed: {},
     watch: {},
     mounted() {
-        // this.drawChart()
+        this.initChart()
     },
     methods: {
+        initChart() {
+            let words = []
+            for (let i in this.keywords) {
+                words.push([i, Math.sqrt(this.keywords[i])])
+            }
+            let options = eval({
+                list: words,
+                color: '#15a4fa',
+                imageShape: require('./assets/img/logo.png'),
+                ellipticity: 1,
+                shape: 'circle',
+                gridSize: 2,
+                shadowOffsetX: 2,
+                shadowOffsetY: 2,
+                shadowBlur: 2,
+                weightFactor: 5,
+                fontWeight: 'normal',
+                fontFamily: 'Times, serif',
+                color: 'random-light',
+                // backgroundColor: '#333',
+                rotateRatio: -10,
+                // minRotation: -90,
+                // maxRotation: 90,
+                drawOutOfBound: false,
+                tooltip: {
+                    show: true,
+                    formatter: function(item) {
+                        return item[0] + ': ' + item[1] + '<br>' + '词云图'
+                    }
+                }
+            })
+            var canvas = document.getElementById('chart')
+            WordCloud(canvas, options)
+        },
         drawChart() {
             let words = []
             for (let i in this.keywords) {
@@ -59,7 +109,6 @@ export default {
             maskImage: './src/logo.png',
             backgroundColor: '#fff'
         })
-
         window.addEventListener('resize', function() {
             wordCloud.resize()
         })
@@ -71,10 +120,11 @@ export default {
 .wordCloud {
     width: 100%;
     height: 100%;
-    #main {
-        width: 100%;
-        height: 100%;
-        border: 1px solid red;
+    #chart {
+        width: 400px;
+        height: 400px;
+        background: url('./assets/img/logo.png') no-repeat;
+        background-size: 100% 100%;
     }
 }
 </style>
