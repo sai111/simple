9e79a13a039af5f675d694674666ac331f90f7ce
黑咖啡 <2452052376@qq.com>
2020-12-09 23:42:06 +0800

    fix: 特殊的树图

---
+++ src/packages/svg/treeMind/entry.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="treeMind-entry">
+    treeMind组件
+    <treeMind />
+  </div>
+</template>
+<script>
+import treeMind from './index.vue'
+export default {
+  name: 'treeMindEntry',
+  components: { treeMind },
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+.treeMind-entry {
+  width: 100%;
+  height: 100%;
+}
+</style>
